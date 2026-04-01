6de16f03569c3c3786346d3fbe4ad6705476cd0b
Tejas Banubakode <banubakodetejas@gmail.com>
2026-04-01 10:10:05 +0000

    station_dimensions created

---
+++ models/station_dimension.sql
@@ -0,0 +1,17 @@
+with bike as (
+
+    select 
+        distinct 
+        start_station_id,
+        start_station_name,
+        start_lat,
+        start_lng
+    
+    from {{ source('demo', 'bike') }} 
+    where ride_id != 'ride_id'
+    
+)
+
+
+select * 
+from bike
