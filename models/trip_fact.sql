44cdbc796edf3ef5877134606c848af0114e3d2d
Tejas Banubakode <banubakodetejas@gmail.com>
2026-04-01 10:27:56 +0000

    trip fat model created

---
+++ models/trip_fact.sql
@@ -0,0 +1,19 @@
+with trips as (
+
+    select
+        ride_id,
+        rideable_type,
+        date(to_timestamp(started_at)) as trip_date,
+        start_station_id,
+        end_station_id,
+        member_csual as member_casual,
+        timestampdiff(second, to_timestamp(started_at), to_timestamp(ended_at)) as  trip_duration 
+
+    from {{ source('demo', 'bike') }}
+
+    where ride_id != 'ride_id'
+  
+)
+
+select * 
+from trips
